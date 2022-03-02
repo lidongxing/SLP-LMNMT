f6648bcd22b85b129a05fc2b4f72bf2848f28c0a
lidongxing <lidx@bnu.edu.cn>
2022-03-02 19:44:48 +0800

    Update task_nmt_iwslt17_en2de.py

--- task_nmt_iwslt17_en2de.py
+++ task_nmt_iwslt17_en2de.py
@@ -1,12 +1,10 @@
 #! -*- coding: utf-8 -*-
-# bert做Seq2Seq任务，采用UNILM方案
-# 介绍链接：https://kexue.fm/archives/6933
-# 数据集：https://github.com/CLUEbenchmark/CLGE 中的CSL数据集
-# 补充了评测指标bleu、rouge-1、rouge-2、rouge-l
+# bert做NMT任务，采用UNILM方案
+#参考bert4keras：https://github.com/bojone/bert4keras
 from __future__ import print_function
 import os
 os.environ['TF_KERAS']= '1'
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import numpy as np
 from tqdm import tqdm
 from bert4keras.backend import keras, K
@@ -28,9 +26,10 @@ batch_size = 32
 epochs = 8
 
 # bert配置
-config_path = '/data/lidongxing/bert4keras-master2/models/multi_cased_L-12_H-768_A-12/bert_config.json'
-checkpoint_path = '/data/lidongxing/bert4keras-master2/models/multi_cased_L-12_H-768_A-12/bert_model.ckpt'
-dict_path = '/data/lidongxing/bert4keras-master2/models/multi_cased_L-12_H-768_A-12/vocab.txt'
+#预处理multi-language BERT
+config_path = '/models/multi_cased_L-12_H-768_A-12/bert_config.json'
+checkpoint_path = '/models/multi_cased_L-12_H-768_A-12/bert_model.ckpt'
+dict_path = '/models/multi_cased_L-12_H-768_A-12/vocab.txt'
 
 
 
@@ -47,9 +46,9 @@ def load_data(filename):
 
 
 # 加载数据集
-train_data = load_data('datasets/iwslt2017/ende/corpus_deen.tsv')
-valid_data = load_data('datasets/iwslt2017/ende/dev2010_deen.tsv')
-#test_data = load_data('datasets/iwslt2016/test_lowcased.tsv')
+train_data = load_data('data/ende/corpus_deen.tsv')
+valid_data = load_data('data/ende/dev2010_deen.tsv')
+#test_data = load_data('data/ende/test2010.tsv')
 
 # 加载并精简词表，建立分词器
 token_dict, keep_tokens = load_vocab(
@@ -136,60 +135,56 @@ class Evaluator(keras.callbacks.Callback):
         # 保存最优
         if logs['loss'] <= self.lowest:
             self.lowest = logs['loss']
-            model.save_weights('./iwslt2017_en2de_model/best_model_deen.weights')
+            model.save_weights('./train_models/best_model_deen.weights')
 
+#train
+if __name__ == '__main__':
 
-#if __name__ == '__main__':
-
-#     evaluator = Evaluator()
-#     train_generator = data_generator(train_data, batch_size)
+     evaluator = Evaluator()
+     train_generator = data_generator(train_data, batch_size)
      # d = train_generator.forfit()
      # print(d.__next__())
      # print(d.__next__())
 
-#     model.fit(
-#         train_generator.forfit(),
-#         steps_per_epoch=len(train_generator),
-#         epochs=epochs,
-#         callbacks=[evaluator]
-#     )
+     model.fit(
+         train_generator.forfit(),
+         steps_per_epoch=len(train_generator),
+         epochs=epochs,
+         callbacks=[evaluator]
+     )
 #
-#else:
-
-#     model.load_weights('./iwslt2017_en2de_model/best_model_deen.weights')
+else:
+    model.load_weights('./train_models/best_model_deen.weights')
 
 # '''''''''''''''''''''test'''''''''''''''''
-model.load_weights('./iwslt2017_en2de_model/best_model_deen.weights')
-#dess = []
-#for (de,en) in valid_data:
-#    des = autotitle.generate(en,topk=4)
-#    dess.append(des)
+model.load_weights('./train_models/best_model_deen.weights')
+dess = []
+for (de,en) in valid_data:
+    des = autotitle.generate(en,topk=4)
+    dess.append(des)
 
-des = autotitle.generate('We need to develop a feminine discourse that not only honors but also implements mercy instead of revenge, collaboration instead of competition, inclusion instead of exclusion',topk=4)
-print(des)
-
-#import codecs
-#def write_trans_result(filename):
-#    with codecs.open(filename,'w') as f:
-#        f.write('\n'.join(dess))
-#write_trans_result('datasets/iwslt2017/ende/dev2010.de.trans')
+import codecs
+def write_trans_result(filename):
+    with codecs.open(filename,'w') as f:
+        f.write('\n'.join(dess))
+write_trans_result('datasets/iwslt2017/ende/dev2010.de.trans')
 
 
 #---------------------get source file-------------
-#import codecs
-#def get_src(filename1,filename2):
+import codecs
+def get_src(filename1,filename2):
 #     """加载数据
 #     单条格式：(标题, 正文)
 #     """
-#     D = []
-#     with open(filename1) as f:
-#         for l in f:
-#             title, content = l.strip().split('\t')
-#             D.append(title)
-#     with open(filename2,'w') as f1:
-#         f1.write('\n'.join(D))
+     D = []
+     with open(filename1) as f:
+         for l in f:
+             title, content = l.strip().split('\t')
+             D.append(title)
+     with open(filename2,'w') as f1:
+         f1.write('\n'.join(D))
 
-#get_src('datasets/iwslt2017/ende/dev2010_deen.tsv','datasets/iwslt2017/ende/dev2010.src.en')
+get_src('datasets/iwslt2017/ende/dev2010_deen.tsv','datasets/iwslt2017/ende/dev2010.src.en')
 #en2de BLEU:5.09
 #de2en BLEU:14.18
 #德语、荷兰-日耳曼语系
